1aacd02ed8886c089c43cda00da4a7bf3c213a4b
ih24n69 <muhammadihsan69@gmail.com>
2025-09-05 08:11:49 +0700

    ubah menu ke bahasa indonesia

--- application/views/app/_layouts/nav-instructor.php
+++ application/views/app/_layouts/nav-instructor.php
@@ -2,7 +2,7 @@
     <a class="c-sidebar__link <?php if($this->uri->segment(2)=='lms_courses' or $this->uri->segment(2)=='lms_courses' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/lms_courses') ?>">
         <span class="c-sidebar__icon">
             <i class="fa fa-send-o"></i>
-        </span>Courses
+        </span>Kelas
     </a>
 </li>
 
@@ -12,7 +12,7 @@
         <a class="c-sidebar__link <?php if($this->uri->segment(2)=='user_invoice' or $this->uri->segment(2)=='user_invoice' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/user_invoice') ?>">
             <span class="c-sidebar__icon">
                 <i class="fa fa-shopping-cart"></i>
-            </span>Invoice
+            </span>Faktur
         </a>
     </li>
 
@@ -20,7 +20,7 @@
         <a class="c-sidebar__link <?php if($this->uri->segment(2)=='user_invoice_history' or $this->uri->segment(2)=='user_invoice_history' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/user_invoice_history') ?>">
             <span class="c-sidebar__icon">
                 <i class="fa fa-clock-o"></i>
-            </span>Invoice History
+            </span>Riwayat Faktur
         </a>
     </li>    
 
@@ -28,7 +28,7 @@
         <a class="c-sidebar__link <?php if($this->uri->segment(2)=='setting_payment' or $this->uri->segment(2)=='setting_payment' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/setting_payment') ?>">
             <span class="c-sidebar__icon">
                 <i class="fa fa-dollar"></i>
-            </span>Payment
+            </span>Pembayaran
         </a>
     </li>
 <?php endif ?>
--- application/views/app/_layouts/nav.php
+++ application/views/app/_layouts/nav.php
@@ -8,18 +8,18 @@
 
 <li class="c-sidebar__item">
 	<a class="c-sidebar__link <?php if($this->uri->segment(2)=='lms_courses' or $this->uri->segment(2)=='lms_courses' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/lms_courses') ?>">
-		<i class="fa fa-send-o u-mr-xsmall"></i>Courses
+		<i class="fa fa-send-o u-mr-xsmall"></i>Kelas
 	</a>
 </li>
 
 <li class="c-sidebar__item has-submenu <?php if(strpos($this->uri->segment(2), "lms_") !== FALSE){echo "is-open";}?>">
 	<a class="c-sidebar__link collapsed" data-toggle="collapse" href="#lms-submenu" aria-expanded="false" aria-controls="lms-submenu">
-		<i class="fa fa-caret-square-o-down u-mr-xsmall"></i>Part of LMS
+		<i class="fa fa-caret-square-o-down u-mr-xsmall"></i>Bagian LMS
 	</a>
 	<ul class="c-sidebar__submenu collapse <?php if(strpos($this->uri->segment(2), "lms_") !== FALSE){echo "show";}?>" id="lms-submenu">
 
 		<li><a class="c-sidebar__link" href="<?php echo base_url('app/lms_category') ?>">
-			Category
+			Kategori
 			<?php if (strpos($this->uri->segment(2), "lms_category") !== FALSE): ?>
 				<i class="fa fa-check u-ml-xsmall"></i>
 			<?php endif ?>
@@ -32,7 +32,7 @@
 			</a>
 		</li>
 		<li><a class="c-sidebar__link" href="<?php echo base_url('app/lms_coupon') ?>">
-			Coupon
+			Kupon
 			<?php if (strpos($this->uri->segment(2), "lms_coupon") !== FALSE): ?>
 				<i class="fa fa-check u-ml-xsmall"></i>
 			<?php endif ?>
@@ -52,19 +52,19 @@
 
 <li class="c-sidebar__item">
 	<a class="c-sidebar__link <?php if($this->uri->segment(2)=='blog_post' or $this->uri->segment(2)=='blog_post' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/blog_post') ?>">
-		<i class="fa fa-send-o u-mr-xsmall"></i>Post
+		<i class="fa fa-send-o u-mr-xsmall"></i>Postingan
 	</a>
 </li>
 
 <li class="c-sidebar__item has-submenu <?php if(strpos($this->uri->segment(2), "blog_") !== FALSE){echo "is-open";}?>">
 	<a class="c-sidebar__link collapsed" data-toggle="collapse" href="#blog-submenu" aria-expanded="false" aria-controls="blog-submenu">
-		<i class="fa fa-caret-square-o-down u-mr-xsmall"></i>Part of Blog
+		<i class="fa fa-caret-square-o-down u-mr-xsmall"></i>Bagian Blog
 	</a>
 	<ul class="c-sidebar__submenu collapse <?php if(strpos($this->uri->segment(2), "blog_") !== FALSE){echo "show";}?>" id="blog-submenu">
 		
 		<?php if ($this->site['blog_comment']['type'] == 'system'): ?>
 			<li><a class="c-sidebar__link" href="<?php echo base_url('app/blog_post_comment') ?>">
-				Comment
+				Komentar
 				<?php if (strpos($this->uri->segment(2), "blog_post_comment") !== FALSE): ?>
 					<i class="fa fa-check u-ml-xsmall"></i><?php endif ?>
 				</a>
@@ -86,40 +86,40 @@
 	</ul>
 </li>
 
-<h4 class="c-sidebar__title">Pages</h4>
+<h4 class="c-sidebar__title">Halaman</h4>
 
 <li class="c-sidebar__item">
 	<a class="c-sidebar__link <?php if($this->uri->segment(2)=='site_pages' or $this->uri->segment(2)=='site_pages' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/site_pages') ?>">
-		<i class="fa fa-file u-mr-xsmall"></i>Pages
+		<i class="fa fa-file u-mr-xsmall"></i>Halaman
 	</a>
 </li>
 
-<h4 class="c-sidebar__title">User</h4>
+<h4 class="c-sidebar__title">Pengguna</h4>
 
 <li class="c-sidebar__item">
 	<a class="c-sidebar__link <?php if($this->uri->segment(2)=='user' or $this->uri->segment(2)=='user' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/user') ?>">
-		<i class="fa fa-user-circle u-mr-xsmall"></i>User
+		<i class="fa fa-user-circle u-mr-xsmall"></i>Pengguna
 	</a>
 </li>
 
 <?php if ($this->site['payment_method'] == 'Manual'): ?>
 <li class="c-sidebar__item">
 	<a class="c-sidebar__link <?php if($this->uri->segment(2)=='user_invoice' or $this->uri->segment(2)=='user_invoice' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/user_invoice') ?>">
-		<i class="fa fa-shopping-cart u-mr-xsmall"></i>Invoice
+		<i class="fa fa-shopping-cart u-mr-xsmall"></i>Faktur
 	</a>
 </li>
 <li class="c-sidebar__item">
 	<a class="c-sidebar__link <?php if($this->uri->segment(2)=='user_invoice_history' or $this->uri->segment(2)=='user_invoice_history' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/user_invoice_history') ?>">
-		<i class="fa fa-clock-o u-mr-xsmall"></i>Invoice History
+		<i class="fa fa-clock-o u-mr-xsmall"></i>Riwayat Faktur
 	</a>
 </li>
 <?php endif ?>
 
-<h4 class="c-sidebar__title">Settings</h4>
+<h4 class="c-sidebar__title">Setelan</h4>
 
 <li class="c-sidebar__item">
 	<a class="c-sidebar__link <?php if($this->uri->segment(2)=='setting_general' or $this->uri->segment(2)=='setting_general' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/setting_general') ?>">
-		<i class="fa fa-cog u-mr-xsmall"></i>General
+		<i class="fa fa-cog u-mr-xsmall"></i>Umum
 	</a>
 </li>
 <li class="c-sidebar__item">
@@ -139,6 +139,6 @@
 </li>
 <li class="c-sidebar__item">
 	<a class="c-sidebar__link <?php if($this->uri->segment(2)=='setting_payment' or $this->uri->segment(2)=='setting_payment' and $this->uri->segment(3) == 'create' ){echo "is-active";}?>" href="<?php echo base_url('app/setting_payment') ?>">
-		<i class="fa fa-dollar u-mr-xsmall"></i>Payment
+		<i class="fa fa-dollar u-mr-xsmall"></i>Pembayaran
 	</a>
 </li>
