998c8a2f2a1be15966db2a179df04d31f1f00deb
ih24n69 <muhammadihsan69@gmail.com>
2025-09-06 08:14:22 +0700

    Fix untuk php7.4
    Message: Trying to access array offset on value of type bool Filename: _layouts/footer-copyright.php Line Number: 8

--- application/views/blog/templates/mediumish/_layouts/footer-copyright.php
+++ application/views/blog/templates/mediumish/_layouts/footer-copyright.php
@@ -1,19 +1,35 @@
 <!-- Begin Footer
 	================================================== -->
 	<div class="footer">
+		<?php
+			// Safeguards untuk menghindari akses offset pada boolean/null
+			$site_title = (is_array($site) && isset($site['title'])) ? $site['title'] : '';
+
+			$footer_pages = (isset($widget['footer_pages']) && is_array($widget['footer_pages'])) ? $widget['footer_pages'] : [];
+			$footer_status = isset($footer_pages['status']) ? $footer_pages['status'] : '';
+			$footer_content = (isset($footer_pages['content']) && is_array($footer_pages['content'])) ? $footer_pages['content'] : [];
+		?>
 		<p class="pull-left">
-			<?php echo $this->lang->line('copyright') ?> <?php echo "©".date('Y '); echo $site['title']; ?>.
-			<?php echo $this->lang->line('rendered_in') ?> {elapsed_time} <?php echo $this->lang->line('seconds') ?>
+			<?php echo $this->lang->line('copyright'); ?>
+			<?php echo " ©" . date('Y ') . $site_title; ?>.
+			<?php echo $this->lang->line('rendered_in'); ?> {elapsed_time} <?php echo $this->lang->line('seconds'); ?>
 		</p>
-		<?php if ($widget['footer_pages']['status'] == 'active'): ?>
+
+		<?php if ($footer_status === 'active'): ?>
 			<p class="pull-right">
-				<?php foreach ($widget['footer_pages']['content'] as $page): ?>
-					<a title='<?php echo $page['title'] ?>' href="<?php echo $page['url'] ?>"><?php echo $page['title'] ?></a>				
-				<?php endforeach ?>
+				<?php foreach ($footer_content as $page): ?>
+					<?php
+						$page_title = is_array($page) && isset($page['title']) ? $page['title'] : '';
+						$page_url   = is_array($page) && isset($page['url']) ? $page['url'] : '#';
+					?>
+					<a title="<?php echo $page_title; ?>" href="<?php echo $page_url; ?>">
+						<?php echo $page_title; ?>
+					</a>
+				<?php endforeach; ?>
 			</p>
-		<?php endif ?>
-		<div class="clearfix">
-		</div>
+		<?php endif; ?>
+
+		<div class="clearfix"></div>
 	</div>
 <!-- End Footer
 ================================================== -->
