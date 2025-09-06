4a7c23aa409b2217a214beadbb6c48692e542f48
ih24n69 <muhammadihsan69@gmail.com>
2025-09-06 09:19:00 +0700

    Fix untuk php7.4
    Message: Trying to access array offset on value of type null Filename: setting_payment/index.php

--- application/views/app/setting_payment/index.php
+++ application/views/app/setting_payment/index.php
@@ -12,7 +12,7 @@
 			<div class="c-card c-card--responsive u-p-zero">
 				<div class="c-card__header o-line">   
 					<h5 class="c-card__title">  
-						<?php  echo $title; ?>
+						<?php echo $title; ?>
 					</h5>   
 					<?php if ($this->session->userdata('app_grade') == 'App'): ?>		
 						<button class="c-btn c-btn--info u-ml-auto u-mr-small c-btn--custom" type="submit">
@@ -27,11 +27,10 @@
 					<?php endif ?>
 
 					<div class="row">
-
 						<div class="col-12">
 
-							<!-- start -->	
-							<div class="c-stage u-m-zero u-border-bottom-zero type-manual" style='<?php echo ($site['payment_method'] == 'Manual') ? '' : 'display:none'; ?>'>
+							<!-- start transaksi -->
+							<div class="c-stage u-m-zero u-border-bottom-zero type-manual" style='<?php echo (isset($site['payment_method']) && $site['payment_method'] == 'Manual') ? '' : 'display:none'; ?>'>
 
 								<div class="c-stage__header o-media u-justify-start cursor-default">
 									<div class="c-stage__icon o-media__img">
@@ -48,7 +47,6 @@
 								</div>
 
 								<div class="c-stage__panel u-p-medium">
-
 									<div class="c-table-responsive">
 										<table class="c-table c-table--highlight" style="display: table">
 											<thead class="c-table__head c-table__head--slim">
@@ -60,45 +58,34 @@
 												</tr>
 											</thead>
 											<tbody>
-												<?php if ($payment['transaction']): ?>
+												<?php if (!empty($payment['transaction'])): ?>
 													<?php foreach ($payment['transaction'] as $transaction): ?>
 														<tr class="c-table__row">
-															<th class="c-table__cell">
-																<?php echo $transaction['type'] ?>
-															</th>
-															<th class="c-table__cell">
-																<?php echo $transaction['account_number'] ?>
-															</th> 
-															<th class="c-table__cell">
-																<?php echo $transaction['receiver'] ?>
-															</th>
+															<th class="c-table__cell"><?php echo $transaction['type'] ?></th>
+															<th class="c-table__cell"><?php echo $transaction['account_number'] ?></th> 
+															<th class="c-table__cell"><?php echo $transaction['receiver'] ?></th>
 															<th class="c-table__cell" style="width: 10%">
 																<button data-type='<?php echo $transaction['type'] ?>' data-account_number='<?php echo $transaction['account_number'] ?>' data-receiver='<?php echo $transaction['receiver'] ?>' data-identity='<?php echo $transaction['identity'] ?>' type="button" class="c-btn c-btn--small c-btn--info c-btn--custom button-payment-transaction" data-toggle="modal" data-target="#form-transaction"> 
 																	<i class="fa fa-edit"></i>
 																</button>
-																<button data-title="apakah Anda yakin ?" data-text="ingin menghapus transaction  <?php echo $transaction['type'] ?>" class="c-btn--custom c-btn--small c-btn c-btn--danger single-action" data-href="<?php echo base_url('app/setting_payment/delete/transaction/'.$transaction['identity']) ?>" type="button"><i class="fa fa-trash"></i></button>
+																<button data-title="apakah Anda yakin ?" data-text="ingin menghapus transaction <?php echo $transaction['type'] ?>" class="c-btn--custom c-btn--small c-btn c-btn--danger single-action" data-href="<?php echo base_url('app/setting_payment/delete/transaction/'.$transaction['identity']) ?>" type="button"><i class="fa fa-trash"></i></button>
 															</th>
 														</tr>
 													<?php endforeach ?>
-												<?php endif ?>
-												<?php if (empty($payment['transaction'])): ?>
+												<?php else: ?>
 													<tr class="c-table__row">
-														<th class="c-table__cell u-text-center" colspan="4">
-															Tidak ada data!
-														</th>
+														<th class="c-table__cell u-text-center" colspan="4">Tidak ada data!</th>
 													</tr>
 												<?php endif ?>
 											</tbody>
 										</table>
 									</div>
-
 								</div>
-
 							</div>
-							<!-- end -->
+							<!-- end transaksi -->
 
-							<!-- start -->
-							<div class="c-stage u-m-zero u-border-top-zero type-manual" style='<?php echo ($site['payment_method'] == 'Manual') ? '' : 'display:none'; ?>'>
+							<!-- start konfirmasi -->
+							<div class="c-stage u-m-zero u-border-top-zero type-manual" style='<?php echo (isset($site['payment_method']) && $site['payment_method'] == 'Manual') ? '' : 'display:none'; ?>'>
 
 								<div class="c-stage__header o-media u-justify-start cursor-default">
 									<div class="c-stage__icon o-media__img">
@@ -115,7 +102,6 @@
 								</div>
 
 								<div class="c-stage__panel u-p-medium">
-
 									<div class="c-table-responsive">
 										<table class="c-table c-table--highlight" style="display: table">
 											<thead class="c-table__head c-table__head--slim">
@@ -126,15 +112,11 @@
 												</tr>
 											</thead>
 											<tbody>
-												<?php if ($payment['confirmation']): ?>
+												<?php if (!empty($payment['confirmation'])): ?>
 													<?php foreach ($payment['confirmation'] as $confirmation): ?>
 														<tr class="c-table__row">
-															<th class="c-table__cell">
-																<?php echo $confirmation['type'] ?>
-															</th>
-															<th class="c-table__cell">
-																<?php echo $confirmation['data'] ?>
-															</th> 
+															<th class="c-table__cell"><?php echo $confirmation['type'] ?></th>
+															<th class="c-table__cell"><?php echo $confirmation['data'] ?></th> 
 															<th class="c-table__cell" style="width: 10%">
 																<button data-type='<?php echo $confirmation['type'] ?>' data-data='<?php echo $confirmation['data'] ?>' data-identity='<?php echo $confirmation['identity'] ?>' type="button" class="c-btn c-btn--small c-btn--info c-btn--custom button-payment-confirmation" data-toggle="modal" data-target="#form-confirmation"> 
 																	<i class="fa fa-edit"></i>
@@ -143,25 +125,19 @@
 															</th>
 														</tr>
 													<?php endforeach ?>
-												<?php endif ?>
-												<?php if (empty($payment['confirmation'])): ?>
+												<?php else: ?>
 													<tr class="c-table__row">
-														<th class="c-table__cell u-text-center" colspan="3">
-															Tidak ada data!
-														</th>
+														<th class="c-table__cell u-text-center" colspan="3">Tidak ada data!</th>
 													</tr>
 												<?php endif ?>
 											</tbody>
 										</table>
 									</div>
-
 								</div>
-
 							</div>
-							<!-- end -->
+							<!-- end konfirmasi -->
 
 						</div>
-
 					</div>
 				</div>
 			</div>
@@ -169,10 +145,8 @@
 		</div>
 
 	</form>
-
 </div>
 
 <?php $this->load->view('app/setting_payment/form-confirmation'); ?>
 <?php $this->load->view('app/setting_payment/form-transaction'); ?>
-
 <?php $this->load->view('app/_layouts/footer'); ?>
