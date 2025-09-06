1760a39bddfece5236973a1956e88585c22f35cb
ih24n69 <muhammadihsan69@gmail.com>
2025-09-06 08:25:22 +0700

    Fix untuk php7.4
    Message: Trying to access array offset on value of type bool Filename: part/post_pagination.php Line Number: 24

--- application/views/blog/templates/pisen/post/part/post_pagination.php
+++ application/views/blog/templates/pisen/post/part/post_pagination.php
@@ -1,44 +1,51 @@
 <div class="another-posts">
     <div class="row no-gutters">
+
+        <!-- Prev Post -->
         <div class="col-12 col-md-6">
-            <?php if ($post['prev_post']['title']): ?>
+            <?php if (isset($post['prev_post']) && is_array($post['prev_post']) && !empty($post['prev_post']['title'])): ?>
                 <div class="another-post_block prev-post">
                     <div class="post-mini-img text-left">
                         <a title="<?php echo $post['prev_post']['title']; ?>" href="<?php echo $post['prev_post']['url'] ?>">
-                            <?php if ($post['prev_post']['image']['thumbnail']): 
-                            //?>   
-                            <img src="<?php echo $post['prev_post']['image']['thumbnail']; ?>" alt="<?php echo $post['prev_post']['title']; ?>">                        
-                            <?php else : ?>
-                               <img src="<?php echo $post['prev_post']['image']['no_image']; ?>" alt="<?php echo $post['prev_post']['title']; ?>">        
-                           <?php endif ?>
-                       </a>
-                   </div>
-                   <div class="post-title">
-                    <p>Previous post</p>
-                    <a title="<?php echo $post['prev_post']['title']; ?>" href="<?php echo $post['prev_post']['url'] ?>"><?php echo $post['prev_post']['title'] ?></a>
+                            <?php if (!empty($post['prev_post']['image']['thumbnail'])): ?>   
+                                <img src="<?php echo $post['prev_post']['image']['thumbnail']; ?>" alt="<?php echo $post['prev_post']['title']; ?>">                        
+                            <?php else: ?>
+                                <img src="<?php echo $post['prev_post']['image']['no_image']; ?>" alt="<?php echo $post['prev_post']['title']; ?>">        
+                            <?php endif ?>
+                        </a>
+                    </div>
+                    <div class="post-title">
+                        <p>Sebelumnya</p>
+                        <a title="<?php echo $post['prev_post']['title']; ?>" href="<?php echo $post['prev_post']['url'] ?>">
+                            <?php echo $post['prev_post']['title'] ?>
+                        </a>
+                    </div>
                 </div>
-            </div>
-        <?php endif ?>
+            <?php endif ?>
+        </div>
+
+        <!-- Next Post -->
+        <div class="col-12 col-md-6">
+            <?php if (isset($post['next_post']) && is_array($post['next_post']) && !empty($post['next_post']['title'])): ?>
+                <div class="another-post_block text-right next-post">
+                    <div class="post-title">
+                        <p>Selanjutnya</p>
+                        <a title="<?php echo $post['next_post']['title']; ?>" href="<?php echo $post['next_post']['url'] ?>">
+                            <?php echo $post['next_post']['title'] ?>
+                        </a>
+                    </div>
+                    <div class="post-mini-img text-right">
+                        <a title="<?php echo $post['next_post']['title']; ?>" href="<?php echo $post['next_post']['url'] ?>">
+                            <?php if (!empty($post['next_post']['image']['thumbnail'])): ?>   
+                                <img src="<?php echo $post['next_post']['image']['thumbnail']; ?>" alt="<?php echo $post['next_post']['title']; ?>">                        
+                            <?php else: ?>
+                                <img src="<?php echo $post['next_post']['image']['no_image']; ?>" alt="<?php echo $post['next_post']['title']; ?>">        
+                            <?php endif ?>                        
+                        </a>
+                    </div>
+                </div>
+            <?php endif ?>
+        </div>
+
     </div>
-    <div class="col-12 col-md-6">
-        <?php if ($post['next_post']['title']): ?>
-            <div class="another-post_block text-right next-post">
-                <div class="post-title">
-                    <p>Next post</p>
-                    <a title="<?php echo $post['next_post']['title']; ?>" href="<?php echo $post['next_post']['url'] ?>"><?php echo $post['next_post']['title'] ?></a>
-                </div>
-                <div class="post-mini-img text-right">
-                    <a title="<?php echo $post['next_post']['title']; ?>" href="<?php echo $post['next_post']['url'] ?>">
-                        <?php if ($post['next_post']['image']['thumbnail']): 
-                        //?>   
-                        <img src="<?php echo $post['next_post']['image']['thumbnail']; ?>" alt="<?php echo $post['next_post']['title']; ?>">                        
-                        <?php else : ?>
-                           <img src="<?php echo $post['next_post']['image']['no_image']; ?>" alt="<?php echo $post['next_post']['title']; ?>">        
-                       <?php endif ?>                        
-                   </a>
-               </div>
-           </div>
-       <?php endif ?>
-   </div>
-</div>
 </div>
