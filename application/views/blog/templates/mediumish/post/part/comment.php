cfdef66c44de547462011ac886623ff2cafdbd4b
ih24n69 <muhammadihsan69@gmail.com>
2025-09-06 08:19:31 +0700

    Fix untuk php7.4
    Message: Trying to access array offset on value of type bool Filename: part/comment.php Line Number: 1

--- application/views/blog/templates/mediumish/post/part/comment.php
+++ application/views/blog/templates/mediumish/post/part/comment.php
@@ -1,7 +1,17 @@
-<?php if ($post['comment_post']['type'] == 'disqus'): ?>
-    <?php echo $post['comment_post']['data']; ?>
-<?php endif ?>
+<?php if (isset($post['comment_post']) && is_array($post['comment_post'])): ?>
+    
+    <?php if (isset($post['comment_post']['type']) && $post['comment_post']['type'] == 'disqus'): ?>
+        <?php echo $post['comment_post']['data'] ?? ''; ?>
+    <?php endif; ?>
 
-<?php if ($post['comment_post']['type'] == 'system'):?>
-   <div class="alert alert-info"><?php echo $this->lang->line('comment_not_support') ?></div>
-<?php endif ?>
+    <?php if (isset($post['comment_post']['type']) && $post['comment_post']['type'] == 'system'): ?>
+        <div class="alert alert-info">
+            <?php echo $this->lang->line('comment_not_support'); ?>
+        </div>
+    <?php endif; ?>
+
+<?php else: ?>
+    <div class="alert alert-warning">
+        <?php echo $this->lang->line('comment_not_available') ?? 'Komentar tidak tersedia.'; ?>
+    </div>
+<?php endif; ?>
